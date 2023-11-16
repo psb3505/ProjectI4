8afc1c7e3bfb7e0f61b09c000b0050904b8d87fb
dlrvus <yws0912@naver.com>
2023-11-16 15:59:59 +0900

    test1
    header, section 1.1

---
+++ Ytest1.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>음식 선호도 조사</title>
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Arial', sans-serif;
+        }
+
+        header {
+            background-color: orange;
+            padding: 10px;
+            text-align: center;
+            color: white;
+        }
+
+        section {
+            text-align: center;
+            padding: 20px;
+        }
+
+        h2 {
+            color: orange;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <h1 style="margin: 0;">I4</h1>
+    </header>
+
+    <section>
+        <h2>선호도 조사</h2>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <label for="food">음식 선택:</label>
+            <select name="food" id="food" required>
+                <option value="피자">피자</option>
+                <option value="스테이크">스테이크</option>
+                <option value="초밥">초밥</option>
+                <!-- 다른 음식 옵션 추가 -->
+            </select>
+            <br>
+            <label for="rating">선호도 (1부터 5까지):</label>
+            <input type="number" name="rating" id="rating" min="1" max="5" required>
+            <br>
+            <input type="submit" value="제출">
+        </form>
+    </section>
+</body>
+
+</html>
