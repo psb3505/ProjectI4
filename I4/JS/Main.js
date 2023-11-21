c91c22bced66b25506a5c8dfb6df49747083d918
SeongBeen <142138080+psb3505@users.noreply.github.com>
2023-11-21 22:33:05 +0900

    네이버 지도 앱 링크 연결 및 자동 검색 기능 추가

--- I4/JS/Main.js
+++ I4/JS/Main.js
@@ -1,3 +1,6 @@
+let foodNameText;
+
+// 음식 추천 select
 function foodNameFiltering() {
     return new Promise((resolve, reject) => {
         fetch('CBFiltering.php', {
@@ -10,6 +13,8 @@ function foodNameFiltering() {
             .then(data => {
                 console.log(data);
                 foodNameWriteText(data);
+                // 받아온 데이터를 로컬 스토리지에 저장
+                localStorage.setItem('foodData', JSON.stringify(data));
                 resolve(data);
                 
             })
@@ -19,6 +24,7 @@ function foodNameFiltering() {
     });
 }
 
+// 음식 추천 이미지 및 이름 화면 반영
 function foodNameWriteText(name) {
     const foodName = document.querySelectorAll('.foodName');
     const foodImageElements = document.querySelectorAll('.foodImage');
@@ -35,6 +41,16 @@ function foodNameWriteText(name) {
 const images = document.querySelectorAll('.foodImage');
 images.forEach((image) => {
     image.addEventListener('click', function () {
+        // 클릭된 이미지의 부모 요소를 찾습니다.
+        const parentDiv = this.closest('.slideitem');
+
+        // 부모 요소에서 h2를 찾습니다.
+        const foodNameElement = parentDiv.querySelector('.foodName');
+
+        // h2의 텍스트를 가져와서 출력하거나 다른 작업을 수행합니다.
+        foodNameText = foodNameElement.textContent;
+        console.log(foodNameText);
+
         document.querySelector('.modal').style.display = 'block';
     });
 });
@@ -51,4 +67,24 @@ window.onclick = function(event) {
     if (event.target == commentModal) {
         commentModal.style.display = 'none';
     }
+}
+
+var naverMapLink1 = document.getElementById('naverMapLink1');
+
+naverMapLink1.addEventListener('click', function () {
+    openNaverMap();
+});
+
+function openNaverMap() {
+    // 네이버 지도 앱을 열기 위한 URL Scheme
+    var naverMapScheme = 'nmap://search?query=';
+
+    // 앱 실행 시도
+    window.location.href = naverMapScheme + encodeURIComponent(foodNameText);
+
+    // 일정 시간(예: 3초)이 지난 후에 네이버 지도 앱이 실행되지 않으면 네이버 지도 웹 버전으로 이동
+    setTimeout(function () {
+        // 웹 버전으로 이동하며 검색 키워드를 추가
+        window.location.href = 'https://map.naver.com/v5/search/' + encodeURIComponent(foodNameText);
+    }, 3000);
 }
