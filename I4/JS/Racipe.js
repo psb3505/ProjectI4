b0917edac3924165357e41a13c44904af685066e
SeongBeen <142138080+psb3505@users.noreply.github.com>
2023-11-24 00:34:11 +0900

    레시피 내용 및 동영상 db에서 가져오기, 동영상 동적으로 생성

--- I4/JS/Racipe.js
+++ I4/JS/Racipe.js
@@ -1,3 +1,58 @@
+// 2. 이 코드는 Iframe Player API를 비동기적으로 로드한다. !!필수!!
+var tag;
+var firstScriptTag;
+var videoIdLink;
+
+// 3. API 코드를 다운로드 받은 다음에 <iframe>을 생성하는 기능 (youtube player도 더불어)
+var player;
+function onYouTubeIframeAPIReady() {
+    console.log('onYouTubeIframeAPIReady():', videoIdLink);
+    player = new YT.Player('player', {
+        height: '250',  //변경가능-영상 높이
+        width: '360',  //변경가능-영상 너비
+        videoId: `${videoIdLink}`,  //변경-영상ID
+        playerVars: {
+            'rel': 0,    //연관동영상 표시여부(0:표시안함)
+            'controls': 1,    //플레이어 컨트롤러 표시여부(0:표시안함)
+            'autoplay' : 0,   //자동재생 여부(1:자동재생 함, mute와 함께 설정)
+            'mute' : 1,   //음소거여부(1:음소거 함)
+            'loop' : 0,    //반복재생여부(1:반복재생 함)
+            'playsinline' : 1,    //iOS환경에서 전체화면으로 재생하지 않게
+            'playlist' : `${videoIdLink}`   //재생할 영상 리스트
+            },
+            events: {
+            'onReady': onPlayerReady, //onReady 상태일 때 작동하는 function이름
+            'onStateChange': onPlayerStateChange //onStateChange 상태일 때 작동하는 function이름
+        }
+    });
+}
+
+// 4. API는 비디오 플레이어가 준비되면 아래의 function을 불러올 것이다.
+function onPlayerReady(event) {
+    console.log('Player is ready!');
+    event.target.playVideo();
+}
+
+// 5. API는 플레이어의 상태가 변화될 때 아래의 function을 불러올 것이다.
+//    이 function은 비디오가 재생되고 있을 때를 가르킨다.(state=1),
+//    플레이어는 6초 이상 재생되고 정지되어야 한다.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        console.log('Player state changed:', event.data);
+        var duration = player.getDuration();
+        
+        // 전체 영상 시간만큼 재생 후 정지
+        setTimeout(function () {
+            done = true;
+            stopVideo();
+        }, duration * 1000); // setTimeout은 밀리초 단위이므로 초로 변환
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
+
 function getRacipeVideoAndContent() {
     // 현재 URL에서 쿼리 문자열을 가져오기
     var queryString = window.location.search;
@@ -27,6 +82,16 @@ function getRacipeVideoAndContent() {
             .then(response => response.json())
             .then(data => {
                 console.log(data);
+                videoIdLink = data[0].url;
+                console.log('videoIdLink:', videoIdLink);
+
+                tag = document.createElement('script');
+
+                tag.src = "https://www.youtube.com/iframe_api";
+                firstScriptTag = document.getElementsByTagName('script')[0];
+                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+                onYouTubeIframeAPIReady(); // 여기서 YouTube API 함수 호출
                 resolve(data);
                 
             })
@@ -35,3 +100,7 @@ function getRacipeVideoAndContent() {
             });
     });
 }
+
+// function createIngredientAndRacipeContent() {
+
+// }
